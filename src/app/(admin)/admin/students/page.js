adb57494ebe7eb3ff5ea376468bbb96b11d8e0cf
Bilal raza <attari1235@gmail.com>
2024-10-26 10:21:56 +0500

    User and Trainer Page Updated

--- src/app/(admin)/admin/students/page.js
+++ src/app/(admin)/admin/students/page.js
@@ -1,7 +1,15 @@
+import { UserTrainerTable } from "@/components/DataTables/UserTable";
+import { UserTrainerModal } from "@/components/Dialogs/UserDialog";
+
 export default function Students() {
   return (
     <div className="min-h-screen p-10">
-      <h1 className="text-4xl font-bold text-center">Students</h1>
+      <div className="flex justify-between my-2">
+        <h1 className="text-3xl font-bold text-center">Students</h1>
+        <UserTrainerModal />
+      </div>
+
+      <UserTrainerTable />
     </div>
   );
 }
